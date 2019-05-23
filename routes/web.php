556dee6dc1ed9fea36d674ecb33abe002b69be40
Adelina <adelinasj2013@gmail.com>
2019-05-23 20:07:03 +0200

    Ruteo comenzado
    He comenzado a crear las rutas básicas que usaré.

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,56 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+
+Route::group(['middleware' => 'auth'], function() {
+
+	Route::get('/', 'InicioController@index');
+
+    Route::group(['prefix' => 'inicio'], function(){
+
+        Route::get('/', 'TodoController@getIndex');
+
+        Route::get('show/{id}', 'TodoController@getShow');
+
+        Route::get('crearConjunto', 'TodoController@getCreate');
+
+        Route::get('editarConjunto/{id}', 'TodoController@getEdit');
+
+        Route::post('create', 'TodoController@postCreate');
+
+        Route::put('edit/{id}', 'TodoController@putEdit');
+
+        Route::put('changeRented/{id}', 'InicioController@changeRented');
+    });
 });
+
+Auth::routes();
+
+Route::get('/home', 'InicioController@index')->name('home');
+
+
+/* Route::group(['middleware' => 'auth'], function() {
+
+	Route::get('/', 'HomeController@index');
+
+    Route::group(['prefix' => 'catalog'], function(){
+
+        Route::get('/', 'CatalogController@getIndex');
+
+        Route::get('show/{id}', 'CatalogController@getShow');
+
+        Route::get('create', 'CatalogController@getCreate');
+
+        Route::get('edit/{id}', 'CatalogController@getEdit');
+
+        Route::post('create', 'CatalogController@postCreate');
+
+        Route::put('edit/{id}', 'CatalogController@putEdit');
+
+        Route::put('changeRented/{id}', 'CatalogController@changeRented');
+    });
+});
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');   */
